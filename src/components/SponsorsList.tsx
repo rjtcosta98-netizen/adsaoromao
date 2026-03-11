d8b462522fd5ee591f55fd05f8e131c1622f984f
rjtcosta98-netizen <rjtcosta98@gmail.com>
2026-03-11 12:33:24 +0000

    final changes

--- src/components/SponsorsList.tsx
+++ src/components/SponsorsList.tsx
@@ -18,11 +18,12 @@ const SPONSORS_DATA = {
     { id: 6, name: "", logo: "", url: "#" },
   ],
   local: [
-    { id: 7, name: "Municipio de Seia", logo: "https://ik.imagekit.io/xqd9lrvbt/logo_cm-seia.png", url: "https://cm-seia.pt/" },
-    { id: 8, name: "Solar do Mimo", logo: "https://ik.imagekit.io/elementgroup/ADSR/Patrocinadores/logo-solardomimo-b.png", url: "https://www.solardomimo.org/" },
-    { id: 9, name: "Agrupamento Escolas de Seia", logo: "https://ik.imagekit.io/elementgroup/ADSR/Patrocinadores/logotipo1.jpg", url: "https://www.aeseia.pt/" },
-    { id: 10, name: "Bombeiros Voluntários São Romão", logo: "https://ik.imagekit.io/elementgroup/ADSR/Patrocinadores/d1df0815-7d16-4855-b150-bc73bb6bc4be.jpeg", url: "https://www.facebook.com/BombeirosVoluntariosdeSaoRomao/" },
-    { id: 11, name: "Junta de Freguesia de S. Romão", logo: "https://ik.imagekit.io/xqd9lrvbt/gemini-2.5-flash-image_melhora-me_este_logo_sem_mudar_nada_e_remove_o_fundo-2.jpg", url: "#" },
+    { id: 7, name: "Junta de Freguesia de S. Romão", logo: "https://ik.imagekit.io/xqd9lrvbt/gemini-2.5-flash-image_melhora-me_este_logo_sem_mudar_nada_e_remove_o_fundo-2.jpg", url: "#" },
+    { id: 8, name: "Municipio de Seia", logo: "https://ik.imagekit.io/xqd9lrvbt/logo_cm-seia.png", url: "https://cm-seia.pt/" },
+    { id: 9, name: "Solar do Mimo", logo: "https://ik.imagekit.io/elementgroup/ADSR/Patrocinadores/logo-solardomimo-b.png", url: "https://www.solardomimo.org/" },
+    { id: 10, name: "Agrupamento Escolas de Seia", logo: "https://ik.imagekit.io/elementgroup/ADSR/Patrocinadores/logotipo1.jpg", url: "https://www.aeseia.pt/" },
+    { id: 11, name: "Bombeiros Voluntários São Romão", logo: "https://ik.imagekit.io/elementgroup/ADSR/Patrocinadores/d1df0815-7d16-4855-b150-bc73bb6bc4be.jpeg", url: "https://www.facebook.com/BombeirosVoluntariosdeSaoRomao/" },
+
   ]
 };
 
