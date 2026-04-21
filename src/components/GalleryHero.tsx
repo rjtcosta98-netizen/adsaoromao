6f648da74095a51215a4fb604e0245375e7234ea
rjtcosta98-netizen <rjtcosta98@gmail.com>
2026-04-21 12:12:16 +0100

    fix

--- src/components/GalleryHero.tsx
+++ src/components/GalleryHero.tsx
@@ -55,7 +55,7 @@ export const GalleryHero: React.FC<GalleryHeroProps> = ({ onNavigate }) => {
               {/* Featured Image */}
               <div className="aspect-video relative group cursor-pointer overflow-hidden">
                  <img 
-                    src="/images/JOGOSSENIORES/FORNO/07.jpg" 
+                    src="/images/JOGOSSENIORES/FORNOS/07.jpg" 
                     alt="Destaque" 
                     loading="lazy"
                     width={600}
